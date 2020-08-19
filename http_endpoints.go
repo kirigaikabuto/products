871b7a731f68d10b2d79b2e6925bc1532d883a1d
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-08-19 23:58:22 +0600

    feat:add sql for create and getting data

--- http_endpoints.go
+++ http_endpoints.go
@@ -6,8 +6,8 @@ import (
 )
 
 type HttpEndpointsFactory interface {
-	ListProductsEndpoint() func(w http.ResponseWriter,r *http.Request)
-	CreateProductEndpoint() func(w http.ResponseWriter,r *http.Request)
+	ListProductsEndpoint() func(w http.ResponseWriter, r *http.Request)
+	CreateProductEndpoint() func(w http.ResponseWriter, r *http.Request)
 	//GetProductByIdEndpoint(idParam string) func(w http.ResponseWriter,r *http.Request)
 	//UpdateProductEndpoint(ipParam string) func(w http.ResponseWriter,r *http.Request)
 	//DeleteProductEndpoint(idParam string) func(w http.ResponseWriter,r *http.Request)
@@ -25,42 +25,43 @@ func NewHttpEndpoints(productService ProductService) HttpEndpointsFactory {
 	return &httpEndpointsFactory{productService: productService}
 }
 
-func(httpFac *httpEndpointsFactory) ListProductsEndpoint() func(w http.ResponseWriter,r *http.Request) {
-	return func(w http.ResponseWriter,r *http.Request) {
+func (httpFac *httpEndpointsFactory) ListProductsEndpoint() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		listProductReq := &ListProductCommand{}
 		if r.Header.Get("Content-Type") == "application/json" {
 			err := json.NewDecoder(r.Body).Decode(listProductReq)
 			if err != nil {
-				respondJSON(w,http.StatusInternalServerError,&customError{err.Error()})
+				respondJSON(w, http.StatusInternalServerError, &customError{err.Error()})
 				return
 			}
 		}
 		data, err := listProductReq.Exec(httpFac.productService)
 		if err != nil {
-			respondJSON(w,http.StatusInternalServerError,&customError{err.Error()})
+			respondJSON(w, http.StatusInternalServerError, &customError{err.Error()})
 			return
 		}
-		respondJSON(w,http.StatusCreated,data)
+		respondJSON(w, http.StatusCreated, data)
 	}
 }
-func(httpFac *httpEndpointsFactory) CreateProductEndpoint() func(w http.ResponseWriter,r *http.Request){
-	return func(w http.ResponseWriter,r *http.Request) {
+func (httpFac *httpEndpointsFactory) CreateProductEndpoint() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		createProductReq := &CreateProductCommand{}
 		if r.Header.Get("Content-Type") == "application/json" {
 			err := json.NewDecoder(r.Body).Decode(createProductReq)
 			if err != nil {
-				respondJSON(w,http.StatusInternalServerError,&customError{err.Error()})
+				respondJSON(w, http.StatusInternalServerError, &customError{err.Error()})
 				return
 			}
 		}
 		data, err := createProductReq.Exec(httpFac.productService)
 		if err != nil {
-			respondJSON(w,http.StatusInternalServerError,&customError{err.Error()})
+			respondJSON(w, http.StatusInternalServerError, &customError{err.Error()})
 			return
 		}
-		respondJSON(w,http.StatusCreated,data)
+		respondJSON(w, http.StatusCreated, data)
 	}
 }
+
 //GetProductByIdEndpoint(idParam string) func(w http.ResponseWriter,r *http.Request)
 //UpdateProductEndpoint(ipParam string) func(w http.ResponseWriter,r *http.Request)
 //DeleteProductEndpoint(idParam string) func(w http.ResponseWriter,r *http.Request)
