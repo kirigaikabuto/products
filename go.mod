e5aab335e18ee7eb5657669a2ca1c08cdb30e565
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-08-20 16:15:42 +0600

    feat: add endpoints

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/kirigaikabuto/products
 
 go 1.13
 
-require github.com/lib/pq v1.8.0
+require (
+	github.com/gorilla/mux v1.7.5-0.20200711200521-98cb6bf42e08 // indirect
+	github.com/lib/pq v1.8.0
+)
--- http_endpoints.go
+++ http_endpoints.go
@@ -2,14 +2,16 @@ package products
 
 import (
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 )
 
 type HttpEndpointsFactory interface {
 	ListProductsEndpoint() func(w http.ResponseWriter, r *http.Request)
 	CreateProductEndpoint() func(w http.ResponseWriter, r *http.Request)
-	//GetProductByIdEndpoint(idParam string) func(w http.ResponseWriter,r *http.Request)
-	//UpdateProductEndpoint(ipParam string) func(w http.ResponseWriter,r *http.Request)
+	GetProductByIdEndpoint(idParam string) func(w http.ResponseWriter, r *http.Request)
+	UpdateProductEndpoint(idParam string) func(w http.ResponseWriter,r *http.Request)
 	//DeleteProductEndpoint(idParam string) func(w http.ResponseWriter,r *http.Request)
 }
 
@@ -62,8 +64,59 @@ func (httpFac *httpEndpointsFactory) CreateProductEndpoint() func(w http.Respons
 	}
 }
 
-//GetProductByIdEndpoint(idParam string) func(w http.ResponseWriter,r *http.Request)
-//UpdateProductEndpoint(ipParam string) func(w http.ResponseWriter,r *http.Request)
+func (httpFac *httpEndpointsFactory) GetProductByIdEndpoint(idParam string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		idStr, ok := vars[idParam]
+		if !ok {
+			respondJSON(w, http.StatusBadGateway, &customError{"id is provided"})
+			return
+		}
+		id, err := strconv.ParseInt(idStr, 10, 10)
+		if err != nil {
+			respondJSON(w, http.StatusBadGateway, &customError{"id should be number"})
+			return
+		}
+		cmd := &GetProductByIdCommand{Id: id}
+		result, err := cmd.Exec(httpFac.productService)
+		if err != nil {
+			respondJSON(w, http.StatusInternalServerError, &customError{err.Error()})
+			return
+		}
+		respondJSON(w, http.StatusOK, result)
+	}
+}
+
+func (httpFac *httpEndpointsFactory) UpdateProductEndpoint(idParam string) func(w http.ResponseWriter,r *http.Request){
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		idStr, ok := vars[idParam]
+		if !ok {
+			respondJSON(w, http.StatusBadGateway, &customError{"id is provided"})
+			return
+		}
+		id, err := strconv.ParseInt(idStr, 10, 10)
+		if err != nil {
+			respondJSON(w, http.StatusBadGateway, &customError{"id should be number"})
+			return
+		}
+		updateProductReq := &UpdateProductCommand{}
+		if r.Header.Get("Content-Type") == "application/json" {
+			err := json.NewDecoder(r.Body).Decode(updateProductReq)
+			if err != nil {
+				respondJSON(w, http.StatusInternalServerError, &customError{err.Error()})
+				return
+			}
+		}
+		updateProductReq.Id = id
+		result, err := updateProductReq.Exec(httpFac.productService)
+		if err != nil {
+			respondJSON(w, http.StatusInternalServerError, &customError{err.Error()})
+			return
+		}
+		respondJSON(w, http.StatusOK, result)
+	}
+}
 //DeleteProductEndpoint(idParam string) func(w http.ResponseWriter,r *http.Request)
 
 func respondJSON(w http.ResponseWriter, status int, payload interface{}) {
